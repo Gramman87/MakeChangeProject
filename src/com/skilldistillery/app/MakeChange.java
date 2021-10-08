c776a2659253a383eb38eb8b86ddc52ec576e1ff
Graham Anderson <gramman87@gmail.com>
2021-10-08 16:54:27 -0600

    while loop & menu

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -1,10 +1,40 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static Scanner scanner = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		boolean businessHours = true;
+		
+		while (businessHours) {
+			printMenu();
+			int userInput = scanner.nextInt();
+			
+			switch (userInput) {
+			case 1:
+				change();
+				break;
+			case 2:
+				System.out.println("Program Terminating... Goodbye.");
+				businessHours = false;
+			}
+		}
+
+
+	
+	public static void printMenu() {
+		System.out.println();
+		System.out.println("========= MENU =========");
+		System.out.println("|                      |");
+		System.out.println("|   1. Customer Trans. |");
+		System.out.println("|   3. Exit            |");
+		System.out.println("|                      |");
+		System.out.println("========================");
+		System.out.println();
 	}
+	
 
 }
